9087562fe4f69dce6dff6d6c8b03023a58acfd21
Wolf Chen <andy11259@yahoo.com.tw>
2019-05-30 10:03:52 +0800

    add ROS workspace

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wolf/ros_ws/arduino_sensor/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.1/lib64"
+export PATH="/opt/ros/kinetic/bin:/usr/local/cuda-9.1/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/home/wolf/bin:/home/wolf/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/wolf/ros_ws/arduino_sensor/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/wolf/ros_ws/arduino_sensor/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/wolf/ros_ws/arduino_sensor/catkin_ws/src:$ROS_PACKAGE_PATH"
